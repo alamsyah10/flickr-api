b89439f1c689cec9f3caf3472bfd7821a2b65a0e
alamsyah10 <alamsyah.imanudin0209@gmail.com>
2022-08-08 14:37:22 +0800

    first commit

---
+++ src/main/java/com/flickrapi/flickrapitest/FlickrApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.flickrapi.flickrapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flickrapi/flickrapitest/FlickrApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flickrapi.flickrapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickrApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
