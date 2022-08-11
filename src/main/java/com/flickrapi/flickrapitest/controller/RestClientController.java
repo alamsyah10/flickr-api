664be9e8dd6ab62dc37250822ecf51032ae9dd14
alamsyah10 <alamsyah.imanudin0209@gmail.com>
2022-08-11 10:44:41 +0800

    add search by title

--- src/main/java/com/flickrapi/flickrapitest/controller/RestClientController.java
+++ src/main/java/com/flickrapi/flickrapitest/controller/RestClientController.java
@@ -1,11 +1,12 @@
 package com.flickrapi.flickrapitest.controller;
 
 import com.flickrapi.flickrapitest.dto.Items;
-import com.flickrapi.flickrapitest.dto.JsonFlickrFeed;
 import com.flickrapi.flickrapitest.service.RestClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/flickr-data")
@@ -21,18 +22,22 @@ public class RestClientController {
 
     @GetMapping
     public Items[] getItems() {
-        ResponseEntity<JsonFlickrFeed> data = service.getData("");
-        return data.getBody().getItems();
+        Items[] data = service.getData("","");
+        return data;
     }
 
     @GetMapping("/feed")
-    public Items[] getItemsByParamsTag(@RequestParam(required = false, name = "tag") String tag) {
-        ResponseEntity<JsonFlickrFeed> data = service.getData(tag);
-        return data.getBody().getItems();
+    public Items[] getItemsByParamsTag(
+            @RequestParam(required = false, name = "tag") String tag,
+            @RequestParam(required = false, name = "title") String title) {
+        Items[] data = service.getData(tag, title);
+        return data;
     }
 
-    @GetMapping("/data")
-    public ResponseEntity<JsonFlickrFeed> getData() {
-        return service.getData("");
-    }
+
+
+//    @GetMapping("/data")
+//    public ResponseEntity<JsonFlickrFeed> getData() {
+//        return service.getData("","");
+//    }
 }
--- src/main/java/com/flickrapi/flickrapitest/service/RestClientService.java
+++ src/main/java/com/flickrapi/flickrapitest/service/RestClientService.java
@@ -1,10 +1,13 @@
 package com.flickrapi.flickrapitest.service;
 
+import com.flickrapi.flickrapitest.dto.Items;
 import com.flickrapi.flickrapitest.dto.JsonFlickrFeed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Locale;
 
 @Service
 public class RestClientService {
@@ -13,27 +16,53 @@ public class RestClientService {
 
     public ResponseEntity<String> getDataString() {
         ResponseEntity<String> response = restTemplate.getForEntity(
-            "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1",
+                "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1",
                 String.class
         );
 
         return response;
     }
 
-    public ResponseEntity<JsonFlickrFeed> getData(String param) {
+    public Items[] getData(String tag, String title) {
         JsonFlickrFeed response;
-        if (param == null || param.isEmpty()) {
+        if (tag == null || tag.isEmpty()) {
             response = restTemplate.getForObject(
                     "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1",
                     JsonFlickrFeed.class
             );
         } else {
             response = restTemplate.getForObject(
-                    "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"+"&tags="+param,
+                    "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"+"&tags="+tag,
                     JsonFlickrFeed.class
             );
         }
 
-        return ResponseEntity.ok(response);
+        ResponseEntity<JsonFlickrFeed> data = ResponseEntity.ok(response);
+        int arr_size = 0;
+        if (title == null || title.isEmpty()) {
+            Items[] items = data.getBody().getItems();
+        } else {
+            Items[] items = data.getBody().getItems();
+
+            for (int i=0; i<items.length; i++) {
+                if (items[i].getTitle().toLowerCase(Locale.ROOT).contains(title.toLowerCase())) {
+                    arr_size++;
+                }
+            }
+        }
+        Items[] new_items = data.getBody().getItems();
+        if (arr_size > 0) {
+            Items[] items = data.getBody().getItems();
+            new_items = new Items[arr_size];
+            int index = 0;
+            for (int i=0; i<items.length; i++) {
+                if (items[i].getTitle().toLowerCase(Locale.ROOT).contains(title.toLowerCase())) {
+                    new_items[index] = items[i];
+                    index++;
+                }
+            }
+        }
+
+        return new_items;
     }
 }
